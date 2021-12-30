5453bf75c6af8b2f51f367dfd68de09df322a2a9
bravotango <seattleriot@hotmail.com>
2021-12-30 13:08:24 -0800

    Comments are being added to DB

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,8 +1,27 @@
 const postClickHandler = (post_id) => {
-  console.log('post_id', post_id);
-  //event.preventDefault();
-  console.log('event', event);
   document.location.replace(`/${post_id}`);
 };
 
-const addCommentHandler = () => {};
+const addCommentHandler = async (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const formProps = Object.fromEntries(formData);
+  console.log('formProps', formProps.post_id);
+
+  const post_id = formProps.post_id.trim();
+  const user_id = formProps.user_id.trim();
+  const content = formProps.content.trim();
+
+  const response = await fetch('/api/comments', {
+    method: 'POST',
+    body: JSON.stringify({ content, user_id, post_id }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert('Failed to add comment.');
+  }
+};
